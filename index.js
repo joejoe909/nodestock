0ad7f13e42fa1774247f918fb5e28280b00fafff
Joe <joseph_a_f@hotmail.com>
2020-11-06 12:18:42 -0700

    finished tutorial

--- index.js
+++ index.js
@@ -2,16 +2,52 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
+
+//user body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+//api key pk_9e6011934ba446feb569dd100e601262 
+function call_api(finishedAPI, ticker){
+    request('https://cloud.iexapis.com/stable/stock/'+ ticker + '/quote?token=' + process.env.API_KEY, {json: true}, (err, res, body) => {
+        if(err){return console.log(err);}
+        if(res.statusCode === 200){
+            finishedAPI(body)
+        }
+    });
+}
+
 
 //Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set handlebar routes
+//Set handlebar index route
 app.get('/', function (req, res) {
-    res.render('home', {stuff: 'this is stuff'});
+    call_api(function(doneAPI){
+        
+        res.render('home', {stock: doneAPI});
+
+    });
+});
+
+//Set handlebar index POST route
+app.post('/', function (req, res) {
+    call_api(function (doneAPI) {
+        //posted_stuff = req.body.stock_ticker;
+        res.render('home', { 
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
+});
+
+
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 //set static folder
